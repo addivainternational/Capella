4ac03cac8647fc747f95c0a89f9dbbb3ff46a882
Simon Johansson <simjoh@addiva.se>
2024-11-21 15:17:40 +0100

    Added initial modal views - one for selection of which field to use as requirement text, another as a warning dialog; controlling which requirements to handle etc

--- nalabs_plugin/Nalabs/src/nalabs/handlers/NalabsHandler.java
+++ nalabs_plugin/Nalabs/src/nalabs/handlers/NalabsHandler.java
@@ -1,29 +1,26 @@
 package nalabs.handlers;
 
 import nalabs.views.MainView;
+import nalabs.views.InitialWarningPopup;
+import nalabs.views.StartupSelectionView;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.time.LocalDateTime;
 
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.common.util.EList;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.polarsys.capella.core.model.handler.helpers.CapellaAdapterHelper;
-import org.polarsys.kitalpha.vp.requirements.Requirements.*;
 import org.polarsys.kitalpha.vp.requirements.Requirements.Module;
 import org.polarsys.kitalpha.vp.requirements.Requirements.Requirement;
 
-//import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -40,15 +37,27 @@ public class NalabsHandler extends AbstractHandler {
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
 
 		Collection<Requirement> requirements = getRequirements(selection);
+		
+		IShellProvider shellProvider = PlatformUI.getWorkbench().getModalDialogShellProvider();
+		StartupSelectionView startupSelectionView = new StartupSelectionView(shellProvider.getShell());
+		if (startupSelectionView.open() != Dialog.OK) {
+		    return null;
+		}
+		
+		boolean useDefaultTextField = startupSelectionView.getUseDefaultTextField();
 
-		Collection<Requirement> correctRequirements = getCorrectRequirements(requirements);
-		Collection<Requirement> faultyRequirements = getFaultyRequirements(requirements);
+		Collection<Requirement> correctRequirements = getCorrectRequirements(requirements, useDefaultTextField);
+		Collection<Requirement> faultyRequirements = getFaultyRequirements(requirements, useDefaultTextField);
+		
+		if (faultyRequirements.size() > 0) {
+			InitialWarningPopup initialWarningPopup = new InitialWarningPopup(shellProvider.getShell(), faultyRequirements, useDefaultTextField);
+			if (initialWarningPopup.open() != Dialog.OK) {
+			    return null;
+			}
+		}
 		
 		// Re map Requirements for transfer to NOLABS lib, and analyze
-		Collection<se.addiva.nalabs_core.Requirement> nalabRequirements = analyzeRequirements(correctRequirements);
-		
-		// Re map results to Capella requirements, updating or adding a NOLABS Attribute
-//		notifyFaultyRequirement(faultyRequirements);
+		Collection<se.addiva.nalabs_core.Requirement> nalabRequirements = analyzeRequirements(correctRequirements, useDefaultTextField);
 		
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
         try {
@@ -61,51 +70,51 @@ public class NalabsHandler extends AbstractHandler {
 		return null;
 	}
 	
-	private void notifyFaultyRequirement(Collection<Requirement> faultyRequirements) {
-		for(Requirement requirement : faultyRequirements) {
-			updateNalabsAttribute(requirement, "NALABS Error: Unable to analyze requirement. Missing ID and/or Text");
+	private boolean hasValidRequirementText(Requirement requirement, boolean useDefaultTextField) {
+		if (useDefaultTextField) {
+			return requirement.getReqIFText() != null && getHtmlTagMatcher(requirement.getReqIFText()).find();
+		} else {
+			return requirement.getReqIFLongName() != null;
 		}
 	}
 
-	private Collection<Requirement> getFaultyRequirements(Collection<Requirement> requirements) {
+	private Collection<Requirement> getFaultyRequirements(Collection<Requirement> requirements, boolean useDefaultTextField) {
 		return requirements
 				.stream()
-				.filter(r -> r.getReqIFIdentifier() == null || r.getReqIFText() == null)
-				.collect(Collectors.toList())
-				;
+				.filter(r -> r.getReqIFIdentifier() == null || !hasValidRequirementText(r, useDefaultTextField))
+				.collect(Collectors.toList());
 	}
 
-	private Collection<Requirement> getCorrectRequirements(Collection<Requirement> requirements) {
+	private Collection<Requirement> getCorrectRequirements(Collection<Requirement> requirements, boolean useDefaultTextField) {
 		return requirements
 				.stream()
-				.filter(r -> r.getReqIFIdentifier() != null && r.getReqIFText() != null)
-				.collect(Collectors.toList())
-				;
-	}
-
-	protected void updateNalabsAttribute(Requirement requirement, String value) {
-		Attribute nalabsAttribute = findAttribute(requirement.getOwnedAttributes(), "NALABS");
-		// Set or add NALABS value
-		if(nalabsAttribute != null) {
-			setNalabsAttributeValue(nalabsAttribute, value);
-		} else {
-			addNalabsAttribute(requirement, value);
-		}		
+				.filter(r -> r.getReqIFIdentifier() != null && hasValidRequirementText(r, useDefaultTextField))
+				.collect(Collectors.toList());
 	}
 	
-	protected Collection<se.addiva.nalabs_core.Requirement> analyzeRequirements(Collection<Requirement> requirements) throws IllegalFormatException {
+	private static Matcher getHtmlTagMatcher(String requirementText) {
+		Pattern pattern = Pattern.compile("<p>(.*?)</p>", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(requirementText);
+		return matcher;
+	}
+	
+	protected Collection<se.addiva.nalabs_core.Requirement> analyzeRequirements(Collection<Requirement> requirements, boolean useDefaultTextField) throws IllegalFormatException {
 		
 		List<se.addiva.nalabs_core.Requirement> nalabsRequirements =
-				requirements.stream().map(r -> copyRequirement(r)).collect(Collectors.toList());
+				requirements.stream().map(r -> copyRequirement(r, useDefaultTextField)).collect(Collectors.toList());
 		
 		for (se.addiva.nalabs_core.Requirement requirement : nalabsRequirements)
         {
-			Pattern pattern = Pattern.compile("<p>(.*?)</p>", Pattern.DOTALL);
-			Matcher matcher = pattern.matcher(requirement.text);
-			if (!matcher.find()) {
-				throw new IllegalArgumentException("The requirement text does not contain the <p> html tag");
+			String textString;
+			if (useDefaultTextField) {
+				Matcher matcher = getHtmlTagMatcher(requirement.text);
+				if (!matcher.find()) {
+					throw new IllegalArgumentException("The requirement text does not contain the <p> html tag");
+				}
+				textString = matcher.group(1);
+			} else {
+				textString = requirement.text;
 			}
-			String textString = matcher.group(1);
 	        
 			se.addiva.nalabs_core.TextAnalysis analysis = se.addiva.nalabs_core.TextAnalyzer.AnalyzeText(textString);
 
@@ -132,88 +141,10 @@ public class NalabsHandler extends AbstractHandler {
 		return nalabsRequirements;
 	}
 	
-	protected se.addiva.nalabs_core.Requirement copyRequirement(Requirement source){
-		se.addiva.nalabs_core.Requirement target = new se.addiva.nalabs_core.Requirement(source.getReqIFIdentifier(), source.getReqIFText());
+	protected se.addiva.nalabs_core.Requirement copyRequirement(Requirement source, boolean useDefaultTextField) {
+		se.addiva.nalabs_core.Requirement target = new se.addiva.nalabs_core.Requirement(source.getReqIFIdentifier(), useDefaultTextField ? source.getReqIFText() : source.getReqIFLongName());
 		return target;
 	}
-
-	protected Attribute addNalabsAttribute(Requirement requirement, String value) {
-		RequirementType rt = requirement.getRequirementType();
-		
-		if(rt == null) {
-			return null;
-		}
-		
-		AttributeDefinition ad = findAttributeDefinition(rt.getOwnedAttributes(), "NALABS");
-
-		if(ad == null) {
-			return null;
-		}
-		
-		RequirementsFactory factory = RequirementsFactory.eINSTANCE;
-		StringValueAttribute stringValueAttribute = factory.createStringValueAttribute();
-
-		stringValueAttribute.setDefinition(ad);
-		stringValueAttribute.setValue(value);
-		
-		addRequirementAttribute(requirement, stringValueAttribute);
-		return stringValueAttribute;
-	}
-	
-	protected void setNalabsAttributeValue(Attribute attribute, String value) {
-		EStructuralFeature attributeValueFeature = attribute.eClass().getEStructuralFeature("value");
-		setStructuralFeature(attribute, attributeValueFeature, value);
-	}
-	
-
-	protected AttributeDefinition findAttributeDefinition(Collection<AttributeDefinition> attributes, String id) {
-		for(AttributeDefinition ad : attributes) {
-			if(ad.getReqIFIdentifier().equalsIgnoreCase(id)) {
-				return ad;
-			}
-		}
-		
-		return null;
-	}
-
-	protected Attribute findAttribute(Collection<Attribute> attributes, String id) {
-		for(Attribute a : attributes) {
-			if(a.getDefinition().getReqIFIdentifier().equalsIgnoreCase(id)) {
-				return a;
-			}
-		}
-		
-		return null;
-	}
-
-	protected void addRequirementAttribute(Requirement requirement, Attribute attribute) {
-		EditingDomain editingDomain = TransactionUtil.getEditingDomain(requirement);
-		if(editingDomain == null) {
-			editingDomain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain();
-		}
-		
-		editingDomain.getCommandStack().execute(new RecordingCommand((TransactionalEditingDomain) editingDomain) {
-	        @Override
-	        protected void doExecute() {
-	    		requirement.getOwnedAttributes().add(attribute);	        	
-	        }
-	    });
-	}
-
-	protected void setStructuralFeature(EObject object, EStructuralFeature feature, Object value) {
-		EditingDomain editingDomain = TransactionUtil.getEditingDomain(object);
-		if(editingDomain == null) {
-			editingDomain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain();
-		}
-		
-		editingDomain.getCommandStack().execute(new RecordingCommand((TransactionalEditingDomain) editingDomain) {
-	        @Override
-	        protected void doExecute() {
-	    		object.eSet(feature, value);
-	        }
-	    });
-	}
-	
 
 	/*
 	 * Collects Requirements from selection, also getting from Modules if selected. 
---
+++ nalabs_plugin/Nalabs/src/nalabs/views/StartupSelectionView.java
@@ -0,0 +1,72 @@
+package nalabs.views;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.FontDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+
+public class StartupSelectionView extends Dialog {
+
+	private boolean useDefaultTextField;
+	private Button textButton;
+	
+    public StartupSelectionView(Shell parentShell) {
+        super(parentShell);
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite container = (Composite) super.createDialogArea(parent);
+        GridLayout gridLayout = new GridLayout(1, false); 
+        gridLayout.marginHeight = 20;
+        gridLayout.marginWidth = 30;
+        container.setLayout(gridLayout);
+
+        Label labelTitle = new Label(container, SWT.NONE | SWT.TOP);
+		GridData labelGridData = new GridData();
+		labelGridData.heightHint = 40;
+		labelTitle.setLayoutData(labelGridData);
+		FontDescriptor boldDescriptor = FontDescriptor.createFrom(labelTitle.getFont()).setStyle(SWT.BOLD).setHeight(16);
+		labelTitle.setFont(boldDescriptor.createFont(labelTitle.getDisplay()));
+		labelTitle.setText("Welcome to the Requirement Smell Detector");
+		
+		Label infoLabel = new Label(container, SWT.NONE);
+		infoLabel.setText("Which requirement field do you want to use as the requirement text?");
+		
+		Composite radioButtonsComposite = new Composite(container, SWT.NONE);
+		radioButtonsComposite.setLayout(new GridLayout(1, false));
+
+	    Button longNameButton = new Button(radioButtonsComposite, SWT.RADIO);
+	    longNameButton.setText("Long Name");
+	    longNameButton.setSelection(false);
+
+	    textButton = new Button(radioButtonsComposite, SWT.RADIO);
+	    textButton.setText("Text field");
+	    textButton.setSelection(true);
+	    
+	    Label recommendedChoiceLabel = new Label(container, SWT.NONE);
+	    recommendedChoiceLabel.setText("Please note that 'Text field' is the recommended choice.");
+	    
+	    Label elseLongNameLabel = new Label(container, SWT.NONE);
+	    elseLongNameLabel.setText("However, if 'Long Name' has been used for the set of requirements, then use that.");
+
+        return container;
+    }
+    
+    @Override 
+    public boolean close() {
+    	useDefaultTextField = textButton.getSelection();
+    	return super.close();
+    }
+    
+    public boolean getUseDefaultTextField() {
+    	return useDefaultTextField;
+    }
+
+}
